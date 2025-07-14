92d962a8f1b22ccf9c5f8ce5995b6950e33d4c16
Vojtech-Benda <vojtech.benda@atlas.cz>
2025-07-14 11:11:15 +0200

    fixed importing functions

--- src/process.py
+++ src/process.py
@@ -1,19 +1,8 @@
-from functools import partial
 from typing import Callable, Any
 from pathlib import Path
 import importlib.util
 
-# from src.algorithms.dicom_convert import dcm2other
-# from src.algorithms.comp2comp import comp2comp
-# from src.algorithms.segment_eye_socket import segment_eye_socket
 from src.utils import read_env
-
-# PROCESS_DISPATCH = {
-#     "dcm2mha": partial(dcm2other, output_datatype="mha"),
-#     "dcm2nifti": partial(dcm2other, output_datatype="nifti"),
-#     "comp2comp": partial(comp2comp, pipeline="spine_muscle_adipose"),
-#     "seg_ct_eye_socket": seg_ct_eye_socket
-# }
 
 
 INTERNAL_PROCESSES = {
@@ -40,8 +29,9 @@ class ProcessDispatcher():
                 print(f"external process '{process}' missing executable/script path")
                 continue
                         
-            exec_path = Path(paths['exec_path']).absolute().expanduser() 
-            script_path = Path(paths['script_path']).absolute().expanduser()
+            exec_path = Path(paths['exec_path']).expanduser() 
+            script_path = Path(paths['script_path']).expanduser()
+            func_path = Path(paths['func_path'])
             
             if not exec_path.is_file():
                 print(f"external process '{process}' executable path not found/not file '{exec_path}'")
@@ -53,18 +43,21 @@ class ProcessDispatcher():
               
             # self.processes[process] = {'exec_path': exec_path, 'script_path': script_path}
                         
-            module_name, module = self.import_method(process, script_path)
+            module_name, module = self.import_method(process, func_path)
             
             self.processes[process] = getattr(module, module_name)
             print(f"registered external {process} function")
         print(f"registered processes: {", ".join(self.processes.keys())}")
     
-    def import_method(self, process: str, script_path: Path):
-        # print(f"importing {process} function")
-        module_name = script_path.stem
-        spec = importlib.util.spec_from_file_location(module_name, script_path)
-        module = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(module)
+    def import_method(self, process: str, func_path: Path):
+        module_name = func_path.stem
+        try:
+            spec = importlib.util.spec_from_file_location(module_name, func_path)
+            module = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(module)
+        except RuntimeError as err:
+            print(err)
+            return None, None
         return module_name, module
 
     # def run_process(self, process_name: str, input_paths: list[str], output_dir: str, options: dict[str, Any] = {}):
